9a98ccb9fb265202dec06768bf9636cbc58af248
sunshine <sunshine>
2019-06-12 21:22:56 +0800

    commit

--- provider1/src/main/java/com/sunshine/springcloud/provider1/module/controller/ProviderController.java
+++ provider1/src/main/java/com/sunshine/springcloud/provider1/module/controller/ProviderController.java
@@ -1,11 +1,16 @@
 package com.sunshine.springcloud.provider1.module.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProviderController {
+    // 测试从config配置中心获取的值
+    @Value("${name}")
+    private String configName ;
     private static int requestCount = 0 ;
     @RequestMapping(value = "provider1" , method = RequestMethod.GET)
     public String provider(String param) throws InterruptedException {
@@ -18,5 +23,11 @@ public class ProviderController {
     public String testZuul(){
         System.out.println(requestCount++);
         return "testZuul_test" ;
+    }
+
+
+    @GetMapping("getConfig")
+    public String getConfig(){
+        return configName ;
     }
 }
